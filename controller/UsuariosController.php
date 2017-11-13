896b39cd4bd0a2f65144dd02a4c059fc774bca80
Carlos Cabrera Gentile <carloscgentile@gmail.com>
2017-11-13 20:44:05 -0300

    ABM lista Usuarios

--- controller/UsuariosController.php
+++ controller/UsuariosController.php
@@ -9,13 +9,77 @@ class UsuariosController extends SecuredController{
     $this->usuariosModel = new UsuariosModel();
   }
   public function mostrarListaUsuarios($value="")
-{
-  $superAdmin=false;
-  if (isset($_SESSION['superAdmin'])) { // pregunto si tengo un usuario
-    $superAdmin = true;
+  {
+      if ($this->superAdmin()) {
+      $usuarios = $this->usuariosModel->getUsuarios();
+
+      $usuarios_editados = [];
+          foreach ($usuarios as $usuario) {
+            if ($usuario['superAdmin'] == 1) {
+               $usuario['tipo'] = "Administrador";
+            }
+            else{
+               $usuario['tipo'] = "Usuario";
+            }
+            array_push($usuarios_editados,$usuario);
+          }
+
+      $this->usuariosView->mostrarUsuarios($usuarios_editados);
+      }
   }
-  $usuarios = $this->usuariosModel->getUsuarios();
-  $this->usuariosView->mostrarUsuarios($usuarios);
-}
+
+  /*FUNCION QUE BORRA USUARIOS*/
+  public function borrarUsuario()
+  {
+    if ($this->superAdmin()) {
+      if (isset($_POST['id_usuario'])) {
+      $id = $_POST['id_usuario'];
+      /*PREGUNTAR SI ES SUPER USUARIO Y DENEGARLO $usuario[0]['superUsuario'] != 1*/
+      $this->usuariosModel->deleteUsuario($id);
+      $usuarios = $this->usuariosModel->getUsuarios();
+      $usuarios_editados = [];
+      foreach ($usuarios as $usuario) {
+        if ($usuario['superAdmin'] == 1) {
+           $usuario['tipo'] = "Administrador";
+        }
+        else{
+           $usuario['tipo'] = "Usuario";
+        }
+        array_push($usuarios_editados,$usuario);
+      }
+      $this->usuariosView->mostrarUsuarios($usuarios_editados);
+      }
+    }
+  }
+
+  public function editarPermisoUsuario()
+  {
+      if ($this->superAdmin()) {
+      if (isset($_POST['id_usuario'])) {
+      $id = $_POST['id_usuario'];
+      $permiso = $_POST['permisoUsuario'];
+      if ($permiso == 1) {
+          $permiso = 0;
+      }
+      else{
+        $permiso = 1;
+      }
+      /*PREGUNTAR SI ES SUPER USUARIO Y DENEGARLO $usuario[0]['superUsuario'] != 1*/
+      $this->usuariosModel->asignarPermisoUsuario($id, $permiso);
+      $usuarios = $this->usuariosModel->getUsuarios();
+      $usuarios_editados = [];
+      foreach ($usuarios as $usuario) {
+        if ($usuario['superAdmin'] == 1) {
+           $usuario['tipo'] = "Administrador";
+        }
+        else{
+           $usuario['tipo'] = "Usuario";
+        }
+        array_push($usuarios_editados,$usuario);
+      }
+      $this->usuariosView->mostrarUsuarios($usuarios_editados);
+      }
+    }
+  }
 }
 ?>
--- model/UsuariosModel.php
+++ model/UsuariosModel.php
@@ -1,11 +1,23 @@
 <?php
 class UsuariosModel extends Model
 {
-/*FUNCION PARA TRAER LOS PRODUCTOS DE LA TABLA*/
+/*FUNCION PARA TRAER LOS USUARIOS DE LA TABLA*/
   function getUsuarios(){
     $sentencia = $this->db->prepare("select * from usuario");//usuario es la tabla de la BBDD
     $sentencia->execute();
     return $sentencia->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+  /*FUNCION PARA BORRAR USUARIOS DE LA TABLA*/
+  function deleteUsuario($id){
+    $sentencia = $this->db->prepare("delete from usuario where id_usuario=?");
+    $sentencia->execute([$id]);
+  }
+
+   /*FUNCION PARA ASIGNAR PERMISO USUARIO DE LA TABLA*/
+  function asignarPermisoUsuario($id, $permiso){
+    $sentencia = $this->db->prepare("UPDATE usuario SET superAdmin=? WHERE id_usuario=?");
+    $sentencia->execute([$permiso, $id]);
   }
 }
 ?>
